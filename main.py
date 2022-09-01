8a96e86f1a8903bb98fe7c010a8c8acca13b4a0c
Dipam Sen <funplanet.m4f@gmail.com>
2022-09-01 13:11:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from notion.client import NotionClient
 
 # Obtain the `token_v2` value by inspecting your browser cookies on a logged-in (non-guest) session on Notion.so
 client = NotionClient(
-    token_v2="71d1e858a1e83142bf8f51c5d504ddca1e1d2d275a570169ab57873a148a817d53c71cd9e85c44174899a1f7946b4c7bbc381953c8ca0010345fe49bad8603ac33473fd2392871d872eaea5252eb")
+    token_v2="")
 
 # Replace this URL with the URL of the page you want to edit
 page = client.get_block(
